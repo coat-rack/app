556c28f710649f7fa70712dc85f20e97235b23be
Logan Dam <dam.loganza@gmail.com>
2024-03-20 22:40:43 +0100

    :construction: define api shape

--- packages/sdk/src/index.ts
+++ packages/sdk/src/index.ts
@@ -10,3 +10,13 @@ export interface App {
    */
   Entry: React.ComponentType
 }
+
+type PromiseArray<T> = Promise<Array<T>>
+type Query = {}
+
+export interface Db<T> {
+  get: (type: string, key: string) => Promise<T>
+  upsert: (type: string, key: string, value: T) => Promise<T>
+  delete: (type: string, key: string) => Promise<void>
+  query: (type: string, query?: Query) => PromiseArray<T>
+}
