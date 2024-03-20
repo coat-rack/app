9d0aaf961221d80a3ec82f879f66443b7f82f0d2
Logan Dam <dam.loganza@gmail.com>
2024-03-20 22:40:12 +0100

    :construction: iframe comms established

--- apps/sandbox/src/Sandbox.tsx
+++ apps/sandbox/src/Sandbox.tsx
@@ -1,10 +1,30 @@
+import { useEffect } from "react"
 import { useApp } from "./dynamic"
 
 function Sandbox() {
-  const url =
-    new URLSearchParams(window.location.search).get("url") || undefined
+  const queryString = new URLSearchParams(window.location.search)
+  const host = queryString.get("host") || undefined
+  const url = queryString.get("url") || undefined
   const app = useApp(url)
   const App = app?.Entry
+
+  useEffect(() => {
+    const handler = (event: MessageEvent) => {
+      if (event.origin != host) {
+        return
+      }
+      console.log("sandbox: got message", event.data)
+    }
+    window.addEventListener("message", handler)
+
+    return () => window.removeEventListener("message", handler)
+  }, [])
+
+  if (!host) {
+    return null
+  }
+
+  window.parent?.postMessage({ testing: 123 }, host)
 
   return App && <App />
 }
--- apps/web/src/routes/apps/$id.lazy.tsx
+++ apps/web/src/routes/apps/$id.lazy.tsx
@@ -1,21 +1,40 @@
-import { createLazyFileRoute } from "@tanstack/react-router"
-
 import { Layout } from "@/layout"
 import { trpcReact } from "@/trpc"
+import { createLazyFileRoute } from "@tanstack/react-router"
+import { useEffect } from "react"
 
 export const Route = createLazyFileRoute("/apps/$id")({
   component: Index,
 })
 
 function Index() {
+  const sandboxHost = "http://localhost:5000"
+  const host = window.location.origin
   const { id } = Route.useParams()
   const { data } = trpcReact.apps.get.useQuery({ id })
+
+  useEffect(() => {
+    const handler = (event: MessageEvent) => {
+      if (event.origin != sandboxHost) {
+        return
+      }
+      console.log("web: got message from sandbox", event.data)
+      event.source?.postMessage("hello from web", {
+        targetOrigin: event.origin,
+      })
+    }
+    window.addEventListener("message", handler)
+
+    return () => window.removeEventListener("message", handler)
+  }, [])
 
   if (!data) {
     return null
   }
 
-  const url = `http://localhost:5000/?url=${encodeURIComponent(data.url)}`
+  const url = `${sandboxHost}/?host=${encodeURIComponent(
+    host,
+  )}&url=${encodeURIComponent(data.url)}`
 
   return (
     <Layout title={data?.id || "Loading"}>
