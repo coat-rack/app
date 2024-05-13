715dbfb970d426761366e776e8a344ee743e1bad
Nabeel Valley <36758308+nabeelvalley@users.noreply.github.com>
2024-05-13 20:31:01 +0200

    :sparkles: cache static assets

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,15 +1,29 @@
 import { TanStackRouterVite } from "@tanstack/router-vite-plugin"
 import react from "@vitejs/plugin-react-swc"
 import { defineConfig } from "vite"
+import { VitePWA } from "vite-plugin-pwa"
 
 import tsconfigPaths from "vite-tsconfig-paths"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [tsconfigPaths(), react(), TanStackRouterVite()],
-
+  plugins: [
+    tsconfigPaths(),
+    react(),
+    TanStackRouterVite(),
+    VitePWA({
+      registerType: "autoUpdate",
+      strategies: "generateSW",
+      workbox: {
+        globPatterns: ["**"],
+      },
+    }),
+  ],
   build: {
     target: "esnext",
+  },
+  preview: {
+    port: 5000,
   },
   server: {
     port: 4000,
