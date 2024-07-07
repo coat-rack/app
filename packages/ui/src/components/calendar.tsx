ae31f953839d7b6e696a048921bd4635ebb3b335
Nabeel Valley <36758308+nabeelvalley@users.noreply.github.com>
2024-07-07 15:12:03 +0200

    :lipstick: consistent border on calendar component

--- packages/ui/src/components/calendar.tsx
+++ packages/ui/src/components/calendar.tsx
@@ -18,6 +18,7 @@ function Calendar({
       showOutsideDays={showOutsideDays}
       className={cn("p-3", className)}
       classNames={{
+        root: "rounded-none border-primary !border-2 ",
         months: "flex flex-col sm:flex-row space-y-4 sm:space-x-4 sm:space-y-0",
         month: "space-y-4",
         caption: "flex justify-center pt-1 relative items-center",
