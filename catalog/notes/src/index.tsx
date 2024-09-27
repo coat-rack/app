555d064b1b02ed0ba5f697291298a46276a5c63a
Logan Dam <dam.loganza@gmail.com>
2024-09-27 13:16:04 +0200

    :sparkle: change save to icon, add delete button

--- catalog/notes/src/index.tsx
+++ catalog/notes/src/index.tsx
@@ -1,5 +1,5 @@
 import { App, DbRecord } from "@repo/sdk"
-
+import { useRefresh } from "@repo/sdk/hooks"
 import { Button } from "@repo/ui/components/button"
 import { useEffect, useState } from "react"
 import { NoteEditor } from "./NoteEditor"
@@ -12,6 +12,8 @@ export const Notes: App<Note> = {
    */
   Entry: ({ db }) => {
     const [notes, setNotes] = useState([] as DbRecord<Note>[])
+    const [signal, doRefresh] = useRefresh()
+    const getNotes = () => db.query<Note>().then((res) => setNotes(res))
 
     const newNote = async () => {
       await db.create({
@@ -21,22 +23,25 @@ export const Notes: App<Note> = {
       getNotes()
     }
 
-    const getNotes = () => db.query<Note>().then((res) => setNotes(res))
-
     useEffect(() => {
+      console.log(signal)
       getNotes()
-    }, [])
+    }, [signal])
 
     return (
       <>
         <h1>notes</h1>
         <Button onClick={newNote}>New note</Button>
-        <pre>{JSON.stringify(notes)}</pre>
         <div className="md:grid-cols:3 grid grid-cols-1 gap-4">
           <div>
             {notes.length ? (
               notes.map((note) => (
-                <NoteEditor db={db} noteId={note.id} key={note.id} />
+                <NoteEditor
+                  db={db}
+                  noteId={note.id}
+                  key={note.id}
+                  onNoteChanged={doRefresh}
+                />
               ))
             ) : (
               <div>No notes yet</div>
