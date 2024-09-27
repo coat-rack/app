8abe95b7dac08a52ec083a83641c28bdc5b1d657
Logan Dam <dam.loganza@gmail.com>
2024-09-27 15:28:53 +0200

    :construction: add dirty indicator

--- catalog/notes/src/NoteEditor.tsx
+++ catalog/notes/src/NoteEditor.tsx
@@ -13,9 +13,16 @@ interface NoteEditorProps {
 
 export function NoteEditor({ noteId, db, onNoteChanged }: NoteEditorProps) {
   const [note, setNote] = useState<Note | undefined>(undefined)
+  const [dirty, setDirty] = useState<boolean>(false)
+
   useEffect(() => {
     db.get(noteId).then((res) => res && setNote(res.data))
   }, [noteId])
+
+  const changeNote = (note: Note) => {
+    setDirty(true)
+    setNote(note)
+  }
 
   const save = async () => {
     if (note) {
@@ -28,6 +35,7 @@ export function NoteEditor({ noteId, db, onNoteChanged }: NoteEditorProps) {
     if (note) {
       await db.delete(noteId)
       onNoteChanged?.(true)
+      setDirty(false)
     }
   }
 
@@ -39,13 +47,14 @@ export function NoteEditor({ noteId, db, onNoteChanged }: NoteEditorProps) {
     <div className="grid grid-cols-1 gap-4">
       <Input
         value={note?.title}
-        onChange={(e) => setNote({ ...note, title: e.target.value })}
+        onChange={(e) => changeNote({ ...note, title: e.target.value })}
       />
       <Textarea
         value={note?.contents}
-        onChange={(e) => setNote({ ...note, contents: e.target.value })}
+        onChange={(e) => changeNote({ ...note, contents: e.target.value })}
       />
-      <div className="flex flex-row justify-end gap-1">
+      <div className="flex flex-row place-items-center justify-end gap-2">
+        {dirty && <span className="text-xs italic">unsaved changes</span>}
         <Button onClick={remove}>
           <Trash className="h-4 w-4 fill-current"></Trash>
         </Button>
