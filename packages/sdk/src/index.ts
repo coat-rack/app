283b7541272f539538bf416f9930f34c02d39cd8
Nabeel Valley <36758308+nabeelvalley@users.noreply.github.com>
2024-12-26 18:07:35 +0100

    :sparkles: define some sdk utils for styling spaces

--- packages/sdk/src/index.ts
+++ packages/sdk/src/index.ts
@@ -1,34 +1,7 @@
-import { AppData } from "@repo/data/models"
-import React from "react"
-
-export type Entry<D = unknown> = React.ComponentType<{ db: Db<D> }>
-
-export interface App<D = unknown> {
-  /**
-   *  The Entrypoint for the app
-   */
-  Entry: Entry<D>
-}
-
-export interface Manifest {
-  /**
-   * The name of the app
-   */
-  name: string
-}
-
-type PromiseArray<T> = Promise<Array<T>>
-
-export type DbRecord<T> = Pick<AppData, "id" | "space" | "timestamp"> & {
-  data: T
-}
-
-export interface Db<T = unknown> {
-  get: <O extends T = T>(key: string) => Promise<DbRecord<O> | undefined>
-  update: <O extends T = T>(key: string, value: O) => Promise<DbRecord<O>>
-  create: <O extends T = T>(value: O) => Promise<DbRecord<O>>
-  delete: (key: string) => Promise<void>
-  query: <O extends T = T>(query?: Partial<O>) => PromiseArray<DbRecord<O>>
-
-  subscribe: <O extends T = T>(query?: Partial<O>) => PromiseArray<DbRecord<O>>
-}
+export {
+  ProvideSpace,
+  getSpaceStyles,
+  useSpace,
+  withSpaceStyles,
+} from "./space"
+export type { App, Db, DbRecord, Entry, Manifest } from "./types"
