caf02caff045f8c89cd0d6af5f7388922d827bc1
Logan Dam <dam.loganza@gmail.com>
2025-03-14 10:43:28 +0100

    :construction: add wip budget app

---
+++ catalog/budget/src/data.ts
@@ -0,0 +1,16 @@
+export interface Category {
+  name: string
+  assignedAmount: number
+  spentAmount: number
+}
+
+export interface CategoryGroup {
+  name: string
+  categories: Category[]
+}
+
+export interface BudgetModel {
+  categoryGroups: CategoryGroup[]
+  startDate: Date
+  endDate: Date
+}
