e1f216a189e94f4ddb8f15fbb223b4a1f2479871
Nabeel Valley <36758308+nabeelvalley@users.noreply.github.com>
2025-03-14 11:49:04 +0100

    :sparkles: init docs package

---
+++ docs/astro.config.mjs
@@ -0,0 +1,33 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+import tailwind from '@astrojs/tailwind';
+
+import react from '@astrojs/react';
+
+// https://astro.build/config
+export default defineConfig({
+    integrations: [starlight({
+        title: 'Coat Rack',
+        social: {
+            github: 'https://github.com/coat-rack',
+        },
+        sidebar: [
+            {
+                label: 'Guides',
+                items: [
+                    // Each item here is one entry in the navigation menu.
+                    { label: 'Example Guide', slug: 'guides/example' },
+                ],
+            },
+            {
+                label: 'Reference',
+                autogenerate: { directory: 'reference' },
+            },
+        ],
+        customCss: ['./src/tailwind.css'],
+        components: {
+            Hero: './src/components/Hero.astro'
+        }
+		}), tailwind({ applyBaseStyles: false }), react()],
+});
