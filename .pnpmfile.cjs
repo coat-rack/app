0502fcd64aac046d63b1726dbb105b763d845446
Logan Dam <dam.loganza@gmail.com>
2025-05-15 20:20:32 +0200

    :bug: fix using dependencies for devDependencies, add pkg name

--- .pnpmfile.cjs
+++ .pnpmfile.cjs
@@ -3,21 +3,23 @@ module.exports = {
     // Replace all internal dependencies with the "workspace:*" version during actual installation
     // We need to keep actual versions in the package file to ensure that
     readPackage: (pkg) => {
-      console.log("Replacing internal packages with workspace:* via coat-rack/.pnpmfile.cjs")
-        
-      for (const [name] of Object.entries(pkg.dependencies)){
+      console.log(
+        `Replacing internal packages with workspace:* via coat-rack/.pnpmfile.cjs ${pkg.name}`,
+      )
+
+      for (const [name] of Object.entries(pkg.dependencies)) {
         if (name.startsWith("@coat-rack/")) {
-          pkg.dependencies[name] = 'workspace:*'
+          pkg.dependencies[name] = "workspace:*"
         }
       }
-      
-      for (const [name] of Object.entries(pkg.devDependencies)){
+
+      for (const [name] of Object.entries(pkg.devDependencies)) {
         if (name.startsWith("@coat-rack/")) {
-          pkg.dependencies[name] = 'workspace:*'
+          pkg.devDependencies[name] = "workspace:*"
         }
       }
-     
+
       return pkg
-    }
-  }
+    },
+  },
 }
