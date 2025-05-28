e146f5cb7326071c6ca59b58027935c3c046d04b
Logan Dam <dam.loganza@gmail.com>
2025-05-28 21:52:58 +0200

    :fire: yeet tanstack devtools

--- apps/web/src/routes/__root.tsx
+++ apps/web/src/routes/__root.tsx
@@ -1,12 +1,10 @@
 import { Layout } from "@/layout"
 import { createRootRoute, Outlet } from "@tanstack/react-router"
-import { TanStackRouterDevtools } from "@tanstack/router-devtools"
 
 export const Route = createRootRoute({
   component: () => (
     <Layout>
       <Outlet />
-      <TanStackRouterDevtools initialIsOpen={false} position="bottom-right" />
     </Layout>
   ),
 })
